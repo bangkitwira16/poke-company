ec0f6194fe890daba2aabcf1e18a76b5d86bf178
bangkitwira <bangkitwira@gmail.com>
2025-04-21 23:51:00 +0700

    add pokemon modal components

---
+++ src/app/components/pokemon-modal/pokemon-modal.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Pokemon } from '../../models/pokemon';
+import { CommonModule } from '@angular/common';
+import {
+  MatCardContent,
+  MatCardHeader,
+  MatCardModule,
+  MatCardTitle,
+} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-pokemon-modal',
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  templateUrl: './pokemon-modal.component.html',
+  styleUrl: './pokemon-modal.component.scss',
+  standalone: true,
+})
+export class PokemonModalComponent {
+  readonly dialogRef = inject(MatDialogRef<PokemonModalComponent>);
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Pokemon) {}
+
+  closeModal(): void {
+    this.dialogRef.close();
+  }
+}
