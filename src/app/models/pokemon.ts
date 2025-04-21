fc33aad0c8b13a4e9a5244ae5bad03db50f6c902
bangkitwira <bangkitwira@gmail.com>
2025-04-21 23:50:21 +0700

    add pokemon interface

---
+++ src/app/models/pokemon.ts
@@ -0,0 +1,25 @@
+export interface Pokemon {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  sprites: {
+    front_default: string;
+  };
+  types: {
+    slot: number;
+    type: {
+      name: string;
+    };
+  }[];
+  abilities: {
+    ability: {
+      name: string;
+    };
+  }[];
+  moves: {
+    move: {
+      name: string;
+    };
+  }[];
+}
